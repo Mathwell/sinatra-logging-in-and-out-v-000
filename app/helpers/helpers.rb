abd64379de1efec43e15890f9722a4caef1b2d60
Olena Ageyeva <eageeva@hotmail.com>
2017-12-23 21:16:50 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,14 +1,9 @@
 class Helpers
-  def self.current_user(id)
-    User.find(id[:user_id])
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user_id])
   end
 
   def self.is_logged_in?(session)
-    if current_user(session)
-      return true
-    else
-      return false
-
-    end
+    !!session[:user_id]
   end
 end
